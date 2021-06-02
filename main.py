d1852f31b3cc11a4c6579e8cc63d6fb93d87a9b8
eraser1121 <38159483+eraser1121@users.noreply.github.com>
2021-06-02 15:25:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ while LiveCam.isOpened():
 
             else :
                 print("직진")
-		if cv2.waitKey(100)>0 :
+		if cv2.waitKey(100) > 0 :
 			cv2.imwrite('cap_img.jpg', frame)
                 	ser.write(serial.to_bytes([int('3',16)]))
                 	break
