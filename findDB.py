336b9c6a603df5c76a599c1c004101a68b5bee38
최지우 <eraser1121@khu.ac.kr>
2021-06-04 11:55:13 +0900

    elif

--- findDB.py
+++ findDB.py
@@ -1,11 +1,11 @@
 def find_name(text) :
     if text.find('지우')>0 or text.find('최지우')>0 or text.find('최지')>0 :
         return '최지우'
-    else if text.find('길동')>0 or text.find('홍길동')>0 or text.find('홍길')>0 :
+    elif text.find('길동')>0 or text.find('홍길동')>0 or text.find('홍길')>0 :
         return '홍길동'
-    else if text.find('대한')>0 or text.find('강대한')>0 or text.find('강대')>0 :
+    elif text.find('대한')>0 or text.find('강대한')>0 or text.find('강대')>0 :
         return '강대한'
-    else if text.find('민국')>0 or text.find('박민국')>0 or text.find('박민')>0 :
+    elif text.find('민국')>0 or text.find('박민국')>0 or text.find('박민')>0 :
         return '박민국'
     else :
         return 'error : name 데이터가 존재하지 않음' 
@@ -13,11 +13,11 @@ def find_name(text) :
 def find_address(text) :
     if text.find('902')>0 :
         return '최지우'
-    else if text.find('801')>0 :
+    elif text.find('801')>0 :
         return '홍길동'
-    else if text.find('803')>0  :
+    elif text.find('803')>0  :
         return '강대한'
-    else if text.find('702')>0 :
+    elif text.find('702')>0 :
         return '박민국'
     else :
         return 'error : address 데이터가 존재하지 않음'
@@ -25,11 +25,11 @@ def find_address(text) :
 def set_destination(name, address):
     if name == '최지우' or address == '최지우' :
         return 902
-    else if name == '홍길동' or address == '홍길동' :
+    elif name == '홍길동' or address == '홍길동' :
         return 801
-    else if name == '강대한' or address == '강대한' :
+    elif name == '강대한' or address == '강대한' :
         return 803
-    else if name == '박민국' or address == '박민국' :
+    elif name == '박민국' or address == '박민국' :
         return 702
     else :
         return 'error : no destination"
